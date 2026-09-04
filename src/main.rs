b07a657943adb303c08b5ffd0bd890590fae5133
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 03:06:08 +0700

    main.rs: add argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod cli;
 
-fn main() {
-    println!("Hello, world!");
+use cli::{
+    add::add_new,
+    args::{Args, Cli},
+    init::create_file,
+};
+
+use clap::Parser;
+use std;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+
+    match &args.command {
+        Args::Init => {
+            create_file()?;
+        }
+        Args::Add { task } => {
+            add_new(task.to_string())?;
+        }
+    }
+
+    Ok(())
 }
