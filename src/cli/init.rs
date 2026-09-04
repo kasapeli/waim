99c5cdc7ef73da1d2129b6cc02610de1aeb272d7
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 03:05:32 +0700

    init.rs: update

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -1,10 +1,15 @@
-use std::{env, fs, io::Result};
+use std::{
+    env,
+    fs::{self, OpenOptions},
+    io::{Result, Write},
+};
 
 pub fn create_file() -> Result<()> {
     let mut dir = env::current_dir()?;
     dir.push("waim.toml");
 
     fs::File::create_new(&dir)?;
+
     println!("Initialized a waim file at {}", &dir.to_string_lossy());
 
     Ok(())
