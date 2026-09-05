cb5534b11b02711d8912c79441688ed6b27716c8
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 14:28:39 +0700

    list.rs: implement listing

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -1,1 +1,31 @@
-pub fn list_all() {}
+use super::add::Todo;
+
+use std::{env, error::Error, fs};
+
+pub fn list_all() -> Result<(), Box<dyn Error>> {
+    let mut dir = env::current_dir()?;
+    dir.push("waim.toml");
+
+    let read = fs::read_to_string(dir)?;
+    let content: Todo = toml::from_str(&read)?;
+
+    if let Some(tasks) = content.task {
+        let all_tasks = tasks.len();
+        let mut completed_count = 0;
+
+        for task in &tasks {
+            if task.completion {
+                completed_count += 1;
+            }
+
+            let status = if task.completion { "X" } else { "" };
+            println!("[{:<1}] | {:^3} | {:>5}", status, task.id, task.task_name);
+        }
+
+        println!();
+        println!("All tasks: {}", all_tasks);
+        println!("Completed: {}", completed_count);
+    }
+
+    Ok(())
+}
