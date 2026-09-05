d44d6c3bf24c4c781c62ec8cb716f77c178cbb5d
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 17:45:10 +0700

    args.rs: add simple help

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -8,10 +8,19 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Args {
+    /// Initializes WAIM in the current directory
     Init,
+
+    /// Adds a task to WAIM
     Add { task: String },
+
+    /// Lists current tasks (options: all, done, undone)
     List { opt: ListOpt },
+
+    /// Deletes a task by id
     Delete { id: usize },
+
+    /// Toggles a task's completion state
     Toggle { id: usize },
 }
 
