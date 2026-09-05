dbbda1483217ea89bbbc8881dc431be19afc3c11
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 14:28:15 +0700

    args.rs: add arg for list

--- src/cli/args.rs
+++ src/cli/args.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Parser)]
 pub struct Cli {
@@ -10,4 +10,13 @@ pub struct Cli {
 pub enum Args {
     Init,
     Add { task: String },
+    List { opt: ListOpt },
+}
+
+#[derive(ValueEnum, Clone, Copy, Debug, PartialEq, Eq)]
+#[value(rename_all = "lower")]
+pub enum ListOpt {
+    All,
+    Done,
+    Undone,
 }
