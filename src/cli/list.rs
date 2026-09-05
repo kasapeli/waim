d2bc7729175acc66923934f86d5ae044eeaf39b9
kasapeli <yanratanaksambath@gmail.com>
2026-09-05 14:38:37 +0700

    list.rs: add listing for done

--- src/cli/list.rs
+++ src/cli/list.rs
@@ -29,3 +29,21 @@ pub fn list_all() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+pub fn list_done() -> Result<(), Box<dyn Error>> {
+    let mut dir = env::current_dir()?;
+    dir.push("waim.toml");
+
+    let read = fs::read_to_string(dir)?;
+    let content: Todo = toml::from_str(&read)?;
+
+    if let Some(tasks) = content.task {
+        for task in &tasks {
+            if task.completion {
+                println!(" {:^3} | {:>5}", task.id, task.task_name);
+            }
+        }
+    }
+
+    Ok(())
+}
